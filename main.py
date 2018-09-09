46b4f2483c877caa9be14acfc12d9343c04f41f0
Ian Shin <ianshin@huntsman-ve702-3517.apng.wlan.private.upenn.edu>
2018-09-09 08:46:38 -0400

    Added constraints to iOS app

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import serial
+import threading
+from bottle import route, run, template
+import json
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://gluclose-ff5dd.firebaseio.com', None)
+
 
 #ngrok.client.BASE_URL = "http://localhost:8765"
 ser = serial.Serial('/dev/cu.usbmodem1411')  # open serial port
-
+data = {}
+data['data'] = [] 
 while True:
 	line = float(ser.readline().decode("utf-8"))
+		#print(line)
+	data['data'].append({
+	'num': line
+	})
 	print(line)
+	result = firebase.post('/', {'data': line})
 
-ser.close() 
+	#print(printA())
+	#print(str(pyresponse))
+ser.close()
